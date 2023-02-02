12429cbe7033254d07fe983eace97f0884895c3f
Thomas <thomtrepanier@gmail.com>
2023-02-02 15:31:12 -0500

    Removed .only

--- packages/server/app/services/chat-service/chat.service.spec.ts
+++ packages/server/app/services/chat-service/chat.service.spec.ts
@@ -127,7 +127,7 @@ describe('ChatService', () => {
                     clientSocket.emit('channel:newMessage', testChannel, expectedMessage);
                 });
 
-                it.only('should throw error if user not in channel', (done) => {
+                it('should throw error if user not in channel', (done) => {
                     clientSocket.on('error' as any, (err: string, code: number) => {
                         expect(err).to.equal(NOT_IN_CHANNEL);
                         expect(code).to.equal(StatusCodes.FORBIDDEN);
