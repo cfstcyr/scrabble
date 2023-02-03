930bbc28eed0a68831581ddb893c2e154fb21866
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2023-02-02 21:54:19 -0500

    RS: Constant and error strings

---
+++ packages/mobile/lib/constants/create-account-constants.dart
@@ -0,0 +1,11 @@
+const String EMAIL_LABEL_FR = 'Courriel';
+const String USERNAME_LABEL_FR = 'Pseudonyme';
+const String PASSWORD_LABEL_FR = 'Mot de passe';
+const String PASSWORD_MATCH_LABEL_FR = 'Confirmer';
+const String CHECKBOX_SHOW_PASSWORD_LABEL_FR = 'Afficher le mot de passe';
+const String REDIRECT_LOGIN_LABEL_FR = 'Vous connecter à un compte existant';
+const String CREATE_ACCOUNT_LABEL_FR = 'Créer son compte';
+const String PASSWORD_HELPER_TEXT_FR = 'Au moins huit caractères comprenant au moins 1 lettre minuscule, 1 lettre majuscule, 1 chiffre et 1 symbole';
+
+const String USERNAME_REGEX_PATTERN = r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+const String PASSWORD_REGEX_PATTERN = r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
