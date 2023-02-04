c23ebd69861042984be87bf806ff3b95f284ea4a
ahmedsidi9 <70984071+ahmedsidi9@users.noreply.github.com>
2023-02-04 01:41:56 -0500

    login method in auth service

--- packages/mobile/lib/services/account-authentification-service.dart
+++ packages/mobile/lib/services/account-authentification-service.dart
@@ -1,21 +1,21 @@
-import 'dart:convert';
-import 'package:http/http.dart';
 import 'package:mobile/classes/account.dart';
+import 'package:mobile/classes/login.dart';
 import 'package:mobile/controllers/account-authentification-controller.dart';
-import 'package:mobile/environments/environment.dart';
 
 import '../locator.dart';
 
 class AccountAuthenticationService {
   AccountAuthenticationService._privateConstructor();
 
-  static final AccountAuthenticationService _instance = AccountAuthenticationService._privateConstructor();
+  static final AccountAuthenticationService _instance =
+      AccountAuthenticationService._privateConstructor();
 
   factory AccountAuthenticationService() {
     return _instance;
   }
   // final headers = {"Content-type": "application/json"};
-  final accountAuthenticationController = getIt.get<AccountAuthenticationController>();
+  final accountAuthenticationController =
+      getIt.get<AccountAuthenticationController>();
   Future<bool> createAccount(Account account) async {
     return await accountAuthenticationController.createAccount(account);
     // if (res.statusCode == 200) {
@@ -36,6 +36,15 @@ class AccountAuthenticationService {
 
   Future<bool> isUsernameUnique(String username) async {
     return await accountAuthenticationController.isUsernameUnique(username);
+    // if (res.statusCode == 200) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+  }
+
+  Future<bool> login(LoginData credentials) async {
+    return await accountAuthenticationController.login(credentials);
     // if (res.statusCode == 200) {
     //   return true;
     // } else {
