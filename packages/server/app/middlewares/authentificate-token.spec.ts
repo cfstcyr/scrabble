4286ffa5d5b2d56d2eede4d84bb9548031a71d50
rachadchazbek17 <rachadchazbek@gmail.com>
2023-02-06 16:06:20 -0500

    Added middleware Tests

--- packages/server/app/middlewares/authentificate-token.spec.ts
+++ packages/server/app/middlewares/authentificate-token.spec.ts
@@ -2,35 +2,50 @@ import { authenticateToken } from './authentificate-token';
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
 import * as spies from 'chai-spies';
-import { Request, Response } from 'express';
-
+import { NextFunction, Request, Response } from 'express';
+import { HttpException } from '@app/classes/http-exception/http-exception';
+import * as jwt from 'jsonwebtoken';
+import * as sinon from 'sinon';
 
 const expect = chai.expect;
 chai.use(spies);
 chai.use(chaiAsPromised);
 
 describe('authenticateToken', () => {
+    let req: Request;
+    let res: Response;
+    let next: NextFunction;
+
+    beforeEach(() => {
+        req = {
+            headers: {
+                authorization: '',
+            },
+            body: {},
+        } as unknown as Request;
+        res = {} as unknown as Response;
+        next = sinon.spy();
+    });
+
     afterEach(() => {
         chai.spy.restore();
     });
 
-    it('Should throw HTTP execption - 401 if user has invalid token', () => {
-        const resMock = {} as unknown as Response;
-        const reqMock = { headers: { authorisation: 'token' } } as unknown as Request;
-        const next = chai.spy();
-
-        authenticateToken(reqMock, resMock, next)
-        expect(next).to.have.been.called;
+    it('Should throw HTTP exception - 401 if user has invalid token', () => {
+        next = sinon.spy((error: HttpException) => {
+            throw error;
+        }) as unknown as NextFunction;
+        try {
+            authenticateToken(req, res, next);
+        } catch (error) {
+            expect(error).to.be.an.instanceOf(HttpException);
+        }
     });
 
-    it('Should throw HTTP execption - 401 if user has invalid token', () => {
-        const resMock = {} as unknown as Response;
-        const reqMock = { headers: { authorisation: 'Bearer: TOKENNN' } } as unknown as Request;
-        const next = chai.spy();
-
-        authenticateToken(reqMock, resMock, next)
-        expect(next).to.have.been.called;
+    it('should verify token', () => {
+        sinon.stub(jwt, 'verify');
+        req.headers.authorization = 'Bearer valid_token';
+        authenticateToken(req, res, next);
+        expect(req.body).to.have.property('user');
     });
-
-
 });
