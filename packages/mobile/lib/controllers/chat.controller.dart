1c8a6843b2ace5ae209476795673bc25f84da9a6
ahmedsidi9 <70984071+ahmedsidi9@users.noreply.github.com>
2023-02-06 18:41:14 -0500

    Update chat.controller.dart

--- packages/mobile/lib/controllers/chat.controller.dart
+++ packages/mobile/lib/controllers/chat.controller.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:mobile/services/socket.service.dart';
 
+import '../classes/channel.dart';
+import '../classes/chat-message.dart';
 import '../constants/socket-events.dart';
 import '../environments/environment.dart';
 import '../locator.dart';
@@ -9,6 +14,7 @@ class ChatController {
   static final ChatController _instance = ChatController._privateConstructor();
   final String endpoint = "${Environment().config.apiUrl}/channel";
   final socketService = getIt.get<SocketService>();
+  List<types.Message> _messages = [];
   factory ChatController() {
     return _instance;
   }
@@ -16,7 +22,10 @@ class ChatController {
     socketService.initSocket();
   }
 
-  Future<void> sendMessage(String message) async {
+  Future<void> sendMessage(Channel channel, ChatMessage message) async {
+    var json = jsonEncode(message.toJson());
+
+    //TO DO : passe channel et message comme les params de l'event
     socketService.emitEvent(CHANNEL_NEW_MESSAGE, message);
   }
 }
