9543d55bc4f66422151709c92eb12fce9154f3c3
ahmedsidi9 <70984071+ahmedsidi9@users.noreply.github.com>
2023-02-05 20:36:53 -0500

    socket events constants

--- packages/mobile/lib/constants/socket-events.dart
+++ packages/mobile/lib/constants/socket-events.dart
@@ -1,1 +1,6 @@
 const CHANNEL_NEW_MESSAGE = "channel:newMessage";
+const CHANNEL_JOIN = "channel:join";
+const CHANNEL_QUIT = "channel:quit";
+const SERVER_URL = 'http://localhost:3000';
+const SERVER_URL_TEST =
+    'https://dbeea382-de22-4cee-a208-f952a351a71e.mock.pstmn.io';
