1e028d976dac304017a390d20077e5d49f9252ff
Charles-François St-Cyr <charlesf2001@gmail.com>
2023-02-06 23:21:40 -0500

    created abstract controller

---
+++ packages/client/src/app/controllers/abstract-controller.ts
@@ -0,0 +1,23 @@
+import { environment } from 'src/environments/environment';
+
+export class AbstractController {
+    private baseUrl: string;
+
+    constructor(basePath?: `/${string}`) {
+        this.baseUrl = basePath ? this.join(environment.serverUrl, basePath) : environment.serverUrl;
+    }
+
+    protected url(path: `/${string}`): string {
+        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+        return this.join(this.baseUrl, path);
+    }
+
+    private join(base: string, path: string = '/'): string {
+        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+        path = path.endsWith('/') ? path.substring(-1) : path;
+        path = path.startsWith('/') ? path : `/${path}`;
+
+        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+        return `${base.endsWith('/') ? base.substring(-1) : base}${path}`;
+    }
+}
