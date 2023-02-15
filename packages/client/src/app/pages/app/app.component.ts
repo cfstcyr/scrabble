586af35e9aeee7b1a5176ec3820917931d3e8c42
Charles-François St-Cyr <charlesf2001@gmail.com>
2023-02-15 17:04:06 -0500

    remove comments

--- packages/client/src/app/pages/app/app.component.ts
+++ packages/client/src/app/pages/app/app.component.ts
@@ -10,15 +10,12 @@ import { map } from 'rxjs/operators';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnDestroy {
-    // states: typeof InitializeState = InitializeState;
-    // state: InitializeState = InitializeState.Loading;
     states: typeof InitializeState = InitializeState;
     state: Observable<InitializeState>;
     message: Observable<string | undefined>;
     private componentDestroyed$: Subject<boolean> = new Subject();
 
     constructor(private readonly initializer: InitializerService) {
-        // this.initializer.subscribe(this.componentDestroyed$, this.handleNewState.bind(this));
         this.state = this.initializer.state.pipe(map(({ state }) => state));
         this.message = this.initializer.state.pipe(map(({ message }) => message));
     }
@@ -27,8 +24,4 @@ export class AppComponent implements OnDestroy {
         this.componentDestroyed$.next(true);
         this.componentDestroyed$.complete();
     }
-
-    // handleNewState(state: InitializeState): void {
-    //     this.state = state;
-    // }
 }
