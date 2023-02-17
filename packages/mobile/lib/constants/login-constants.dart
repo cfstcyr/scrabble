fcbb13fe69e0f66ca464679d925b2624b03a8801
ahmedsidi9 <70984071+ahmedsidi9@users.noreply.github.com>
2023-02-16 21:26:32 -0500

    commit for merge [ci skip]

--- packages/mobile/lib/constants/login-constants.dart
+++ packages/mobile/lib/constants/login-constants.dart
@@ -1,4 +1,4 @@
 const String LOGIN_LABEL_FR = 'Se connecter';
 const String LOGO_PATH = "images/Scrabble-logo.png";
 const String ALREADY_LOGGED_IN_FR =
-    "Username deja existant ou deja connecté, Veuillez réesayer";
+    "Username déja existant ou déja connecté, Veuillez réesayer";
