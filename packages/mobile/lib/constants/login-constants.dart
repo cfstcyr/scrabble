6548d495606adedd4ecd53d15e283ab86f844580
ahmedsidi9 <70984071+ahmedsidi9@users.noreply.github.com>
2023-02-18 15:47:53 -0500

    Update login-constants.dart

--- packages/mobile/lib/constants/login-constants.dart
+++ packages/mobile/lib/constants/login-constants.dart
@@ -1,4 +1,6 @@
 const String LOGIN_LABEL_FR = 'Se connecter';
 const String LOGO_PATH = "images/Scrabble-logo.png";
 const String ALREADY_LOGGED_IN_FR =
-    "Pseudonyme déja existant ou déja connecté, Veuillez réesayer";
+    "déja connecté, Veuillez réesayer plus tard";
+const String LOGIN_FAILED =
+    "Courriel ou Mot de passe Incorrect, Veuillez réessayer";
