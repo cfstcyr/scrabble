5f3e5f43029829eeb681827c5f4d9a80d89bf298
ahmedsidi9 <70984071+ahmedsidi9@users.noreply.github.com>
2023-02-27 18:22:20 -0500

    Update storage.handler.dart

--- packages/mobile/lib/services/storage.handler.dart
+++ packages/mobile/lib/services/storage.handler.dart
@@ -2,6 +2,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class StorageHandlerService {
   StorageHandlerService._privateConstructor();
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   static final StorageHandlerService _instance =
       StorageHandlerService._privateConstructor();
   factory StorageHandlerService() {
@@ -9,17 +10,17 @@ class StorageHandlerService {
   }
   static const String _tokenKey = 'token';
   Future<String?> getToken() async {
-    final prefs = await SharedPreferences.getInstance();
+    final SharedPreferences prefs = await _prefs;
     return prefs.getString(_tokenKey);
   }
 
   Future<void> setToken(String token) async {
-    final prefs = await SharedPreferences.getInstance();
+    final SharedPreferences prefs = await _prefs;
     await prefs.setString(_tokenKey, token);
   }
 
   Future<void> clearStorage() async {
-    final prefs = await SharedPreferences.getInstance();
+    final SharedPreferences prefs = await _prefs;
     await prefs.clear();
   }
 }
