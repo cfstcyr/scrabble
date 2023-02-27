ba8b8ecc146e404780512aa7a3b66c0685bcf230
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2023-02-26 23:46:04 -0500

    RS: change server logic for new events and flow

--- packages/server/app/services/game-dispatcher-service/game-dispatcher.service.ts
+++ packages/server/app/services/game-dispatcher-service/game-dispatcher.service.ts
@@ -1,17 +1,16 @@
-import { GameConfigData, ReadyGameConfig, ReadyGameConfigWithChannelId } from '@app/classes/game/game-config';
+import { ReadyGameConfig, ReadyGameConfigWithChannelId } from '@app/classes/game/game-config';
 import Room from '@app/classes/game/room';
 import WaitingRoom from '@app/classes/game/waiting-room';
 import { HttpException } from '@app/classes/http-exception/http-exception';
 import Player from '@app/classes/player/player';
 import { ExpertVirtualPlayer } from '@app/classes/virtual-player/expert-virtual-player/expert-virtual-player';
 import {
-    CANNOT_HAVE_SAME_NAME,
     CANT_START_GAME_WITH_NO_REAL_OPPONENT,
     INVALID_PLAYER_ID_FOR_GAME,
+    NO_DICTIONARY_INITIALIZED,
     NO_GAME_FOUND_WITH_ID,
     NO_USER_FOUND_WITH_NAME,
 } from '@app/constants/services-errors';
-// import { ActiveGameService } from '@app/services/active-game-service/active-game.service';
 import { CreateGameService } from '@app/services/create-game-service/create-game.service';
 import DictionaryService from '@app/services/dictionary-service/dictionary.service';
 import { SocketService } from '@app/services/socket-service/socket.service';
@@ -20,8 +19,9 @@ import { StatusCodes } from 'http-status-codes';
 import { Service } from 'typedi';
 import { ChatService } from '@app/services/chat-service/chat.service';
 import { UserId } from '@app/classes/user/connected-user-types';
-import { Group } from '@common/models/group';
-import { convertToGroup } from '@app/utils/convert-to-lobby-data/convert-to-lobby-data';
+import { Group, GroupData } from '@common/models/group';
+import { convertToGroup } from '@app/utils/convert-to-group/convert-to-group';
+import { PublicUser } from '@common/models/user';
 @Service()
 export class GameDispatcherService {
     private waitingRooms: WaitingRoom[];
@@ -30,7 +30,6 @@ export class GameDispatcherService {
     constructor(
         private socketService: SocketService,
         private createGameService: CreateGameService,
-        // private activeGameService: ActiveGameService,
         private dictionaryService: DictionaryService,
         private virtualPlayerService: VirtualPlayerService,
         private readonly chatService: ChatService,
@@ -48,45 +47,46 @@ export class GameDispatcherService {
         return this.virtualPlayerService;
     }
 
-    async createMultiplayerGame(config: GameConfigData, userId: UserId): Promise<Group> {
-        const waitingRoom = await this.createGameService.createMultiplayerGame(config, userId);
-        this.dictionaryService.useDictionary(config.dictionary.id);
+    async createMultiplayerGame(groupData: GroupData, userId: UserId, playerId: string): Promise<Group> {
+        const waitingRoom = await this.createGameService.createMultiplayerGame(groupData, userId, playerId);
+        const dictionaries = this.dictionaryService.getAllDictionarySummaries();
+        if (dictionaries.length < 1) {
+            throw new HttpException(NO_DICTIONARY_INITIALIZED, StatusCodes.INTERNAL_SERVER_ERROR);
+        }
+        waitingRoom.dictionarySummary = dictionaries[0];
+        this.dictionaryService.useDictionary(waitingRoom.dictionarySummary.id);
 
         this.addToWaitingRoom(waitingRoom);
-        this.socketService.addToRoom(config.playerId, waitingRoom.getId());
+        this.socketService.addToRoom(playerId, waitingRoom.getId());
         return convertToGroup(waitingRoom.getConfig(), waitingRoom.getId());
     }
 
-    requestJoinGame(waitingRoomId: string, playerId: string, playerName: string): WaitingRoom {
+    requestJoinGame(waitingRoomId: string, playerId: string, publicUser: PublicUser): WaitingRoom {
         const waitingRoom = this.getMultiplayerGameFromId(waitingRoomId);
 
-        if (waitingRoom.getConfig().player1.name === playerName) {
-            throw new HttpException(CANNOT_HAVE_SAME_NAME, StatusCodes.FORBIDDEN);
-        }
         // TODO: Currently games are acting as private where players need to be accepted
-        waitingRoom.requestingPlayers.push(new Player(playerId, playerName));
+        waitingRoom.requestingPlayers.push(new Player(playerId, publicUser));
         return waitingRoom;
     }
 
     // TODO : Refactor to use player id instead of name
-    async handleJoinRequest(waitingRoomId: string, playerId: string, requestingPlayerName: string, isAccepted: boolean): Promise<[Player, Player[]]> {
+    async handleJoinRequest(waitingRoomId: string, playerId: string, requestingPlayerName: string, isAccepted: boolean): Promise<[Player, Group]> {
         const waitingRoom = this.getMultiplayerGameFromId(waitingRoomId);
         if (waitingRoom.getConfig().player1.id !== playerId) {
             throw new HttpException(INVALID_PLAYER_ID_FOR_GAME, StatusCodes.FORBIDDEN);
         }
-        const requestingPlayers = waitingRoom.requestingPlayers.filter((player) => player.name === requestingPlayerName);
+        const requestingPlayers = waitingRoom.requestingPlayers.filter((player) => player.publicUser.username === requestingPlayerName);
 
         if (requestingPlayers.length === 0) throw new HttpException(NO_USER_FOUND_WITH_NAME, StatusCodes.NOT_FOUND);
 
         const requestingPlayer = requestingPlayers[0];
         if (isAccepted) {
-            await this.chatService.joinChannel(waitingRoom.getGroupChannelId(), playerId);
+            await this.chatService.joinChannel(waitingRoom.getGroupChannelId(), requestingPlayer.id);
             waitingRoom.fillNextEmptySpot(requestingPlayer);
         }
         const index = waitingRoom.requestingPlayers.indexOf(requestingPlayer);
         waitingRoom.requestingPlayers.splice(index, 1);
-        waitingRoom.getPlayers();
-        return [requestingPlayer, waitingRoom.getPlayers()];
+        return [requestingPlayer, waitingRoom.convertToGroup()];
     }
 
     startRequest(waitingRoomId: string, playerId: string): ReadyGameConfigWithChannelId {
@@ -104,34 +104,62 @@ export class GameDispatcherService {
         // TODO: See how we want to handle the names
         const config: ReadyGameConfig = {
             ...waitingRoom.getConfig(),
-            player2: waitingRoom.joinedPlayer2 ? waitingRoom.joinedPlayer2 : new ExpertVirtualPlayer(waitingRoomId, 'VirtualPlayer2'),
-            player3: waitingRoom.joinedPlayer3 ? waitingRoom.joinedPlayer3 : new ExpertVirtualPlayer(waitingRoomId, 'VirtualPlayer3'),
-            player4: waitingRoom.joinedPlayer4 ? waitingRoom.joinedPlayer4 : new ExpertVirtualPlayer(waitingRoomId, 'VirtualPlayer4'),
+            player2: waitingRoom.joinedPlayer2
+                ? waitingRoom.joinedPlayer2
+                : new ExpertVirtualPlayer(waitingRoomId, this.virtualPlayerService.getRandomVirtualPlayerName(waitingRoom.getPlayers())),
+            player3: waitingRoom.joinedPlayer3
+                ? waitingRoom.joinedPlayer3
+                : new ExpertVirtualPlayer(waitingRoomId, this.virtualPlayerService.getRandomVirtualPlayerName(waitingRoom.getPlayers())),
+            player4: waitingRoom.joinedPlayer4
+                ? waitingRoom.joinedPlayer4
+                : new ExpertVirtualPlayer(waitingRoomId, this.virtualPlayerService.getRandomVirtualPlayerName(waitingRoom.getPlayers())),
+            dictionarySummary: waitingRoom.dictionarySummary,
         };
 
         return { ...config, idChannel: waitingRoom.getGroupChannelId() };
     }
 
-    async leaveGroupRequest(waitingRoomId: string, playerId: string): Promise<Player[]> {
+    isPlayerFromAcceptedPlayers(waitingRoomId: string, playerId: string): boolean {
+        const waitingRoom = this.getMultiplayerGameFromId(waitingRoomId);
+        return playerId === waitingRoom.joinedPlayer2?.id || playerId === waitingRoom.joinedPlayer3?.id || playerId === waitingRoom.joinedPlayer4?.id;
+    }
+
+    async leaveGroupRequest(waitingRoomId: string, playerId: string): Promise<Group> {
         const waitingRoom = this.getMultiplayerGameFromId(waitingRoomId);
         switch (playerId) {
             case waitingRoom.joinedPlayer2?.id: {
                 waitingRoom.joinedPlayer2 = undefined;
+
                 break;
             }
             case waitingRoom.joinedPlayer3?.id: {
                 waitingRoom.joinedPlayer3 = undefined;
+
                 break;
             }
             case waitingRoom.joinedPlayer4?.id: {
                 waitingRoom.joinedPlayer4 = undefined;
                 break;
             }
-            default:
+            default: {
                 throw new HttpException(INVALID_PLAYER_ID_FOR_GAME, StatusCodes.FORBIDDEN);
+            }
         }
+
         await this.chatService.quitChannel(waitingRoom.getGroupChannelId(), playerId);
-        return waitingRoom.getPlayers();
+        return waitingRoom.convertToGroup();
+    }
+
+    removeRequestingPlayer(waitingRoomId: string, playerId: string): PublicUser[] {
+        const waitingRoom = this.getMultiplayerGameFromId(waitingRoomId);
+        const requestingPlayers = waitingRoom.requestingPlayers.filter((player) => player.id === playerId);
+        if (requestingPlayers.length === 0) throw new HttpException(INVALID_PLAYER_ID_FOR_GAME, StatusCodes.FORBIDDEN);
+
+        const requestingPlayer = requestingPlayers[0];
+        const index = waitingRoom.requestingPlayers.indexOf(requestingPlayer);
+        waitingRoom.requestingPlayers.splice(index, 1);
+
+        return waitingRoom.requestingPlayers.map((player) => player.publicUser);
     }
 
     async cancelGame(waitingRoomId: string, playerId: string): Promise<void> {
@@ -140,7 +168,7 @@ export class GameDispatcherService {
         if (waitingRoom.getConfig().player1.id !== playerId) {
             throw new HttpException(INVALID_PLAYER_ID_FOR_GAME, StatusCodes.FORBIDDEN);
         }
-        this.dictionaryService.stopUsingDictionary(waitingRoom.getConfig().dictionary.id);
+        this.dictionaryService.stopUsingDictionary(waitingRoom.dictionarySummary.id);
         await this.chatService.emptyChannel(waitingRoom.getGroupChannelId());
 
         const index = this.waitingRooms.indexOf(waitingRoom);
