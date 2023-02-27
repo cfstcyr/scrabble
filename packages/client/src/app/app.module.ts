5a653f742d2f142fe902b9714fd9fe85df0927ab
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2023-02-26 23:20:13 -0500

    RS: Remove unused components and update routes

--- packages/client/src/app/app.module.ts
+++ packages/client/src/app/app.module.ts
@@ -17,30 +17,16 @@ import { CreateWaitingPageComponent } from '@app/pages/create-waiting-page/creat
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { HomePageComponent } from '@app/pages/home-page/home-page.component';
 import { JoinWaitingPageComponent } from '@app/pages/join-waiting-page/join-waiting-page.component';
-import { AdminDictionariesComponent } from './components/admin-dictionaries-component/admin-dictionaries.component';
-import { AdminGameHistoryComponent } from './components/admin-game-history/admin-game-history.component';
-import { AdminHighScoresComponent } from './components/admin-high-scores/admin-high-scores.component';
-import { AdminVirtualPlayersComponent } from './components/admin-virtual-players/admin-virtual-players.component';
 import { ChatBoxComponent } from './components/chatbox/chatbox.component';
 import { CommunicationBoxComponent } from './components/communication-box/communication-box.component';
-import { ConvertDialogComponent } from './components/convert-dialog/convert-dialog.component';
-import { CreateVirtualPlayerComponent } from './components/create-virtual-player-dialog/create-virtual-player-dialog.component';
 import { DefaultDialogComponent } from './components/default-dialog/default-dialog.component';
-import { DeleteDictionaryDialogComponent } from './components/delete-dictionary-dialog/delete-dictionary-dialog.component';
-import { DeleteVirtualPlayerDialogComponent } from './components/delete-virtual-player-dialog/delete-virtual-player-dialog.component';
 import { HighScoreBoxComponent } from './components/high-score-box/high-score-box.component';
 import { IconComponent } from './components/icon/icon.component';
 import { InformationBoxComponent } from './components/information-box/information-box.component';
 import { GroupInfoComponent } from './components/group-info/group-info.component';
-import { ModifyDictionaryComponent } from './components/modify-dictionary-dialog/modify-dictionary-dialog.component';
-import { NameFieldComponent } from './components/name-field/name-field.component';
-import { ObjectiveBoxComponent } from './components/objective-box/objective-box.component';
 import { ObjectiveComponent } from './components/objective/objective.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
 import { TimerSelectionComponent } from './components/timer-selection/timer-selection.component';
-import { UpdateVirtualPlayerComponent } from './components/update-virtual-player-dialog/update-virtual-player-dialog.component';
-import { UploadDictionaryComponent } from './components/upload-dictionary/upload-dictionary.component';
-import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 import { GameCreationPageComponent } from './pages/game-creation-page/game-creation-page.component';
 import { HighScoresPageComponent } from './pages/high-scores-page/high-scores-page.component';
 import { LoadingPageComponent } from './pages/loading-page/loading-page.component';
@@ -60,7 +46,8 @@ import { LoginContainerComponent } from './components/login-container/login-cont
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { HeaderBtnComponent } from './components/header-btn/header-btn.component';
 import { AuthenticationInterceptor } from './middlewares/authentication';
-import { JoinerListContainerComponent } from './components/joiner-list-container/joiner-list-container.component';
+import { RequestingUserContainerComponent } from './components/requesting-user-container/requesting-user-container.component';
+import { GroupRequestWaitingDialogComponent } from './components/group-request-waiting-dialog/group-request-waiting-dialog';
 
 registerLocaleData(localeFr);
 
@@ -90,26 +77,12 @@ registerLocaleData(localeFr);
         GameCreationPageComponent,
         DefaultDialogComponent,
         IconComponent,
-        NameFieldComponent,
         TimerSelectionComponent,
         PageHeaderComponent,
         HighScoresPageComponent,
         HighScoreBoxComponent,
-        ConvertDialogComponent,
-        AdminPageComponent,
-        AdminVirtualPlayersComponent,
-        CreateVirtualPlayerComponent,
-        UpdateVirtualPlayerComponent,
-        DeleteVirtualPlayerDialogComponent,
-        AdminDictionariesComponent,
-        ModifyDictionaryComponent,
-        UploadDictionaryComponent,
-        DeleteDictionaryDialogComponent,
-        AdminHighScoresComponent,
-        AdminGameHistoryComponent,
         DurationPipe,
         ObjectiveComponent,
-        ObjectiveBoxComponent,
         LoadingPageComponent,
         ChatBoxComponent,
         ChatboxContainerComponent,
@@ -122,7 +95,8 @@ registerLocaleData(localeFr);
         LoginWrapperComponent,
         LoginContainerComponent,
         HeaderBtnComponent,
-        JoinerListContainerComponent,
+        RequestingUserContainerComponent,
+        GroupRequestWaitingDialogComponent,
     ],
     imports: [
         AppMaterialModule,
--- packages/server/app/routes/routes.module.ts
+++ packages/server/app/routes/routes.module.ts
@@ -1,12 +1,10 @@
 import {
     AuthentificationController,
     DatabaseController,
-    DictionaryController,
     GameDispatcherController,
     GameHistoriesController,
     GamePlayController,
     HighScoresController,
-    VirtualPlayerProfilesController,
 } from '@app/controllers';
 import { BaseController } from '@app/controllers/base-controller';
 
@@ -14,10 +12,8 @@ export type ClassType<T> = new (...args: unknown[]) => T;
 
 export const PUBLIC_CONTROLLERS: ClassType<BaseController>[] = [AuthentificationController, DatabaseController];
 export const PRIVATE_CONTROLLERS: ClassType<BaseController>[] = [
-    DictionaryController,
     GameDispatcherController,
     GamePlayController,
     GameHistoriesController,
     HighScoresController,
-    VirtualPlayerProfilesController,
 ];
