ae0c1f570e9190a50a3ca0994046041366a1e77b
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2023-02-26 23:14:33 -0500

    RS: Revamp join waiting page for groups and enw socket events

--- packages/client/src/app/pages/join-waiting-page/join-waiting-page.component.ts
+++ packages/client/src/app/pages/join-waiting-page/join-waiting-page.component.ts
@@ -1,22 +1,14 @@
 import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { NavigationStart, Router } from '@angular/router';
-import { PlayerData } from '@app/classes/communication';
 import { Timer } from '@app/classes/round/timer';
 import { DefaultDialogComponent } from '@app/components/default-dialog/default-dialog.component';
 import { getRandomFact } from '@app/constants/fun-facts-scrabble-constants';
-import {
-    DEFAULT_GROUP,
-    DIALOG_BUTTON_CONTENT_REJECTED,
-    DIALOG_BUTTON_CONTENT_RETURN_GROUP,
-    DIALOG_CANCEL_CONTENT,
-    DIALOG_CANCEL_TITLE,
-    DIALOG_REJECT_CONTENT,
-    DIALOG_REJECT_TITLE,
-} from '@app/constants/pages-constants';
+import { DEFAULT_GROUP, DIALOG_BUTTON_CONTENT_RETURN_GROUP, DIALOG_CANCEL_CONTENT, DIALOG_CANCEL_TITLE } from '@app/constants/pages-constants';
 import GameDispatcherService from '@app/services/game-dispatcher-service/game-dispatcher.service';
 import { PlayerLeavesService } from '@app/services/player-leave-service/player-leave.service';
-import GroupInfo from '@common/models/group-info';
+import { Group } from '@common/models/group';
+import { PublicUser } from '@common/models/user';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
@@ -26,11 +18,9 @@ import { takeUntil } from 'rxjs/operators';
     styleUrls: ['./join-waiting-page.component.scss'],
 })
 export class JoinWaitingPageComponent implements OnInit, OnDestroy {
-    currentGroup: GroupInfo;
-    currentName: string;
+    currentGroup: Group;
     funFact: string;
     roundTime: string;
-    opponents: string[] = [];
 
     private componentDestroyed$: Subject<boolean>;
 
@@ -53,7 +43,6 @@ export class JoinWaitingPageComponent implements OnInit, OnDestroy {
         const roundTime: Timer = Timer.convertTime(this.currentGroup.maxRoundTime);
         this.roundTime = `${roundTime.minutes}:${roundTime.getTimerSecondsPadded()}`;
 
-        this.currentName = this.gameDispatcherService.currentName;
         this.funFact = getRandomFact();
 
         this.router.events.pipe(takeUntil(this.componentDestroyed$)).subscribe((event) => {
@@ -62,11 +51,12 @@ export class JoinWaitingPageComponent implements OnInit, OnDestroy {
             }
         });
 
-        this.gameDispatcherService.subscribeToPlayerJoinedGroupEvent(this.componentDestroyed$, (players: PlayerData[]) => this.setOpponents(players));
-        this.gameDispatcherService.subscribeToPlayerLeftGroupEvent(this.componentDestroyed$, (players: PlayerData[]) => this.setOpponents(players));
+        this.gameDispatcherService.subscribeToPlayerJoinedGroupEvent(this.componentDestroyed$, (group: Group) => this.setGroup(group));
+        this.gameDispatcherService.subscribeToPlayerLeftGroupEvent(this.componentDestroyed$, (group: Group) => this.setGroup(group));
 
-        this.gameDispatcherService.subscribeToCanceledGameEvent(this.componentDestroyed$, (hostName: string) => this.hostHasCanceled(hostName));
-        this.gameDispatcherService.subscribeToJoinerRejectedEvent(this.componentDestroyed$, (hostName: string) => this.playerRejected(hostName));
+        this.gameDispatcherService.subscribeToCanceledGameEvent(this.componentDestroyed$, (hostUser: PublicUser) =>
+            this.hostHasCanceled(hostUser.username),
+        );
     }
 
     ngOnDestroy(): void {
@@ -74,35 +64,14 @@ export class JoinWaitingPageComponent implements OnInit, OnDestroy {
         this.componentDestroyed$.complete();
     }
 
-    private setOpponents(players: PlayerData[]) {
-        this.opponents = [];
-        for (const player of players) {
-            if (this.currentName !== player.name && player.name) {
-                this.opponents.push(player.name);
-            }
-        }
+    private setGroup(group: Group) {
+        this.currentGroup = group;
     }
 
     private routerChangeMethod(url: string): void {
         if (url !== '/game') {
             this.playerLeavesService.handleLeaveGroup();
         }
-    }
-
-    private playerRejected(hostName: string): void {
-        this.dialog.open(DefaultDialogComponent, {
-            data: {
-                title: DIALOG_REJECT_TITLE,
-                content: hostName + DIALOG_REJECT_CONTENT,
-                buttons: [
-                    {
-                        content: DIALOG_BUTTON_CONTENT_REJECTED,
-                        redirect: '/group',
-                        closeDialog: true,
-                    },
-                ],
-            },
-        });
     }
 
     private hostHasCanceled(hostName: string): void {
