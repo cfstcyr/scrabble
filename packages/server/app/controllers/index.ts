c8cfe2e050f1f5ac3a6e9aae1a57c003b310a3e8
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2023-02-28 14:41:40 -0500

    RS; Comment out another controller unused

--- packages/server/app/controllers/index.ts
+++ packages/server/app/controllers/index.ts
@@ -2,5 +2,4 @@ export * from './database-controller/database.controller';
 export * from './game-dispatcher-controller/game-dispatcher.controller';
 export * from './game-history-controller/game-history.controller';
 export * from './game-play-controller/game-play.controller';
-export * from './high-score-controller/high-score.controller';
 export * from './authentification-controller/authentification-controller';
