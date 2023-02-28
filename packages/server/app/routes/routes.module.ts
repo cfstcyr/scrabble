c9d9833c65ef9c93565c9f936733387c94c34aa4
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2023-02-28 14:41:04 -0500

    RS: Comment out controlelr that had errors

--- packages/server/app/routes/routes.module.ts
+++ packages/server/app/routes/routes.module.ts
@@ -4,16 +4,10 @@ import {
     GameDispatcherController,
     GameHistoriesController,
     GamePlayController,
-    HighScoresController,
 } from '@app/controllers';
 import { BaseController } from '@app/controllers/base-controller';
 
 export type ClassType<T> = new (...args: unknown[]) => T;
 
 export const PUBLIC_CONTROLLERS: ClassType<BaseController>[] = [AuthentificationController, DatabaseController];
-export const PRIVATE_CONTROLLERS: ClassType<BaseController>[] = [
-    GameDispatcherController,
-    GamePlayController,
-    GameHistoriesController,
-    HighScoresController,
-];
+export const PRIVATE_CONTROLLERS: ClassType<BaseController>[] = [GameDispatcherController, GamePlayController, GameHistoriesController];
