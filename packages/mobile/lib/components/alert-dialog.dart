0e2608b40ba3d93fd67fc7c7e80089bac71720ec
thomtrep <thomtrepanier@gmail.com>
2023-03-01 16:24:24 -0500

    Use app button in dialog box

--- packages/mobile/lib/components/alert-dialog.dart
+++ packages/mobile/lib/components/alert-dialog.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/components/app_button.dart';
 
 import '../routes/navigator-key.dart';
 
 class DialogBoxButtonParameters {
   final String content;
+  final AppButtonTheme theme;
+  final Function()? onPressed;
   final bool? closesDialog;
-  final Function? onPressed;
   final IconData? icon;
 
   DialogBoxButtonParameters(
-      {required this.content, this.closesDialog, this.onPressed, this.icon});
+      {required this.content,
+      required this.theme,
+      this.onPressed,
+      this.closesDialog,
+      this.icon})
+      : assert(onPressed != null ? closesDialog == null : closesDialog != null),
+        assert(closesDialog != null ? onPressed == null : onPressed != null);
 }
 
-void triggerDialogBox(String title, String message, List<DialogBoxButtonParameters> buttons) {
+void triggerDialogBox(
+    String title, String message, List<DialogBoxButtonParameters> buttons) {
   showDialog<void>(
     context: navigatorKey.currentContext!,
     barrierDismissible: false,
@@ -30,12 +39,14 @@ void triggerDialogBox(String title, String message, List<DialogBoxButtonParamete
             ],
           ),
         ),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('Ok'),
-            onPressed: () => onPressed(),
-          ),
-        ],
+        actions: buttons
+            .map((DialogBoxButtonParameters button) => AppButton(
+                  onPressed: button.onPressed ?? () => Navigator.pop(context),
+                  theme: button.theme,
+                  text: button.content,
+                  icon: button.icon,
+                ))
+            .toList(),
       );
     },
   );
