aa3794d4c1b50cac4c9293354e091ad98b94f3a2
ahmedsidi9 <70984071+ahmedsidi9@users.noreply.github.com>
2023-03-02 23:14:07 -0500

    fixed back button edge case

--- packages/mobile/lib/pages/home-page.dart
+++ packages/mobile/lib/pages/home-page.dart
@@ -11,47 +11,57 @@ class HomePage extends StatelessWidget {
       getIt.get<AccountAuthenticationController>();
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          MainTitle(),
-          SizedBox(height: 10),
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SizedBox(width: 10), // c'est un spacing fancy
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => ChatPage()));
-                },
-                child: Text('Amusez vous à clavarder'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  authService.signOut();
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => MainPage()));
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green,
-                  shadowColor: Colors.black,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(3.0),
+    return WillPopScope(
+      onWillPop: () async {
+        handleBackButton();
+        return true;
+      },
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MainTitle(),
+            SizedBox(height: 10),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(width: 10), // c'est un spacing fancy
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => ChatPage()));
+                  },
+                  child: Text('Amusez vous à clavarder'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    authService.signOut();
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => MainPage()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shadowColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(3.0),
+                    ),
+                  ),
+                  child: Text(
+                    SIGNOUT_LABEL_FR,
                   ),
                 ),
-                child: Text(
-                  SIGNOUT_LABEL_FR,
-                ),
-              ),
-              SizedBox(width: 10),
-            ],
-          ),
-        ],
+                SizedBox(width: 10),
+              ],
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  handleBackButton() {
+    authService.signOut();
   }
 }
 
