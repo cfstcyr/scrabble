714422185198052b0e0c485608eca91c00d3f422
ahmedsidi9 <70984071+ahmedsidi9@users.noreply.github.com>
2023-03-03 00:40:38 -0500

    Update login-constants.dart

--- packages/mobile/lib/constants/login-constants.dart
+++ packages/mobile/lib/constants/login-constants.dart
@@ -5,3 +5,8 @@ const String ALREADY_LOGGED_IN_FR =
 const String LOGIN_FAILED = "Identifiants invalides, veuillez réessayer";
 const String AUTHORIZED = "Connecté";
 const String SIGNOUT_LABEL_FR = "Se déconnecter";
+const String BACK_BUTTON_SIGNOUT_CONFIRMATION_FR =
+    "Êtes vous sûr de vouloir revenir en arrière?";
+const String CANCEL_BACK_BUTTON_FR = "Annuler";
+const String CONFIRMATION_BACK_BUTTON_DIALOG_TITLE = "Confirmation";
+const String SIGNOUT_CONFIRMATION_LABEL_FR = "Oui, se déconnecter";
