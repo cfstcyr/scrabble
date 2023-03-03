e760ebf31eda8bc63eabc2fa6e10b966388c2cf5
ahmedsidi9 <70984071+ahmedsidi9@users.noreply.github.com>
2023-03-02 20:38:50 -0500

    fix invalid credentials ui bug

--- packages/mobile/lib/services/user-session.service.dart
+++ packages/mobile/lib/services/user-session.service.dart
@@ -7,7 +7,7 @@ import '../classes/user.dart';
 const SESSION_NOT_INITIALIZED_ASSERT = 'User session not initialized';
 
 class UserSessionService {
-  BehaviorSubject<UserSession?> _userSession = BehaviorSubject<UserSession?>();
+  BehaviorSubject<UserSession?>? _userSession = BehaviorSubject<UserSession?>();
   final storageService = getIt.get<StorageHandlerService>();
   UserSessionService._privateConstructor();
   static final UserSessionService _instance =
@@ -18,20 +18,21 @@ class UserSessionService {
   Future<void> initializeUserSession(UserSession session) async {
     assert(_userSession != null, SESSION_NOT_INITIALIZED_ASSERT);
     await storageService.setToken(session.token);
-    _userSession.add(session);
+    _userSession?.add(session);
   }
 
-  UserSession getSession() {
-    return _userSession.value!;
+  UserSession? getSession() {
+    return _userSession?.value;
   }
 
   void clearUserSession() {
-    _userSession.add(null);
+    assert(_userSession != null, SESSION_NOT_INITIALIZED_ASSERT);
+    _userSession?.add(null);
     storageService.clearStorage();
   }
 
-  String getSessionToken() {
+  String? getSessionToken() {
     assert(_userSession != null, SESSION_NOT_INITIALIZED_ASSERT);
-    return _userSession.value!.token;
+    return _userSession?.value!.token;
   }
 }
