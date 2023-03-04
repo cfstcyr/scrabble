7583d55ff477e4133ef6cec6449eb52f8c5fb647
ahmedsidi9 <70984071+ahmedsidi9@users.noreply.github.com>
2023-03-04 12:37:49 -0500

    Update tile-rack.dart

--- packages/mobile/lib/components/tile/tile-rack.dart
+++ packages/mobile/lib/components/tile/tile-rack.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:mobile/classes/game/game.dart';
 import 'package:mobile/classes/tile/tile.dart' as c;
@@ -174,102 +172,6 @@ class TileRack extends StatelessWidget {
       },
       onAccept: (data) {
         _gameService.getTileRack().placeTile(data, to: index);
-        _currentHoveredTileIndex.add(null);
-      },
-      onMove: (details) {
-        _currentHoveredTileIndex.add(index);
-      },
-      onLeave: (data) {
-        _currentHoveredTileIndex.add(null);
-      },
-    );
-  }
-
-  Widget _buildTile(c.Tile tile, int index) {
-    return Wrap(
-      children: [
-        Draggable(
-            data: tile,
-            onDragStarted: () {
-              _currentTileIndex.add(index);
-              _currentHoveredTileIndex.add(index);
-            },
-            onDragEnd: (details) {
-              _currentTileIndex.add(null);
-              _currentHoveredTileIndex.add(null);
-            },
-            feedback: Tile(
-              tile: tile,
-              size: _tileSize * 1.1,
-              isSelected: true,
-            ),
-            childWhenDragging: StreamBuilder(
-              stream: _currentHoveredTileIndex,
-              builder: (context, snapshot) {
-                return snapshot.data == index ||
-                        snapshot.data == index - 1 ||
-                        snapshot.data == null
-                    ? SizedBox(
-                        height: _tileSize,
-                        width: _tileSize + SPACE_2,
-                      )
-                    : SizedBox();
-              },
-            ),
-            child: Wrap(
-              children: [
-                Stack(
-                  children: [
-                    Tile(
-                      tile: tile,
-                      size: _tileSize,
-                    ),
-                    Wrap(
-                      children: [
-                        _buildTarget(index - 1,
-                            width: _tileSize / 2, height: _tileSize),
-                        _buildTarget(index,
-                            width: _tileSize / 2, height: _tileSize),
-                      ],
-                    ),
-                  ],
-                ),
-                _buildTarget(index,
-                    width: SPACE_2, height: _tileSize, changeOnActive: true)
-              ],
-            )),
-      ],
-    );
-  }
-
-  DragTarget<c.Tile> _buildTarget(int index,
-      {double width = 0, double height = 0, bool changeOnActive = false}) {
-    return DragTarget<c.Tile>(
-      builder: (context, candidateItems, rejectedItems) {
-        return StreamBuilder(
-            stream: _currentTileIndex.stream,
-            builder: (context, currentTileSnapshot) {
-              return StreamBuilder(
-                stream: _currentHoveredTileIndex.stream,
-                builder: (context, currentHoveredTileSnapshot) {
-                  return changeOnActive &&
-                          currentHoveredTileSnapshot.data == index &&
-                          currentTileSnapshot.data != index &&
-                          currentTileSnapshot.data != index + 1
-                      ? Container(
-                          margin: EdgeInsets.symmetric(horizontal: SPACE_2 + 1),
-                          child: Opacity(opacity: 0.25, child: Tile()),
-                        )
-                      : SizedBox(
-                          height: height,
-                          width: width,
-                        );
-                },
-              );
-            });
-      },
-      onAccept: (data) {
-        _gameService.getTileRack().moveTile(data, index);
         _currentHoveredTileIndex.add(null);
       },
       onMove: (details) {
