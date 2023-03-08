3ca012c5b42548e309bebb5422a78aa6bb582b69
thomtrep <thomtrepanier@gmail.com>
2023-03-07 20:48:38 -0500

    dynamic tile count

--- packages/mobile/lib/pages/game-page.dart
+++ packages/mobile/lib/pages/game-page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/classes/game/game.dart';
 import 'package:mobile/components/animation/pulse.dart';
 import 'package:mobile/components/game/game_actions.dart';
 import 'package:mobile/components/game/game_board.dart';
@@ -16,50 +17,56 @@ import '../components/game/game_messages.dart';
 class GamePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.grey.shade100,
-      padding: EdgeInsets.all(SPACE_1),
-      child: SafeArea(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-                child: IntrinsicWidth(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(child: GameBoard()),
-                  TileRack(),
-                ],
-              ),
-            )),
-            SizedBox(
-              width: 425,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  PlayersContainer(),
-                  Row(
+    GameService gameService = getIt.get<GameService>();
+    return StreamBuilder<Game?>(
+      stream: gameService.gameStream,
+      builder: (context, snapshot) {
+        return Container(
+          color: Colors.grey.shade100,
+          padding: EdgeInsets.all(SPACE_1),
+          child: SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    child: IntrinsicWidth(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      Expanded(
-                        child: GameInfo(
-                            value: "12",
-                            name: "Tuiles restantes",
-                            icon: Icons.font_download),
-                      ),
-                      Expanded(
-                        child: GameTimer(),
-                      ),
+                      Expanded(child: GameBoard()),
+                      TileRack(),
                     ],
                   ),
-                  Expanded(child: GameMessages()),
-                  GameActions(),
-                ],
-              ),
+                )),
+                SizedBox(
+                  width: 425,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      PlayersContainer(),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: GameInfo(
+                                value: snapshot.data != null ? snapshot.data!.computeNumberOfTilesLeft() : 0,
+                                name: "Tuiles restantes",
+                                icon: Icons.font_download),
+                          ),
+                          Expanded(
+                            child: GameTimer(),
+                          ),
+                        ],
+                      ),
+                      Expanded(child: GameMessages()),
+                      GameActions(),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      }
     );
   }
 
