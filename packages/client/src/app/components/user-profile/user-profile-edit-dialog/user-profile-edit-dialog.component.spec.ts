da8a2a312f7a16b9784262225931a05979733bcf
Charles-François St-Cyr <charlesf2001@gmail.com>
2023-03-08 22:04:17 -0500

    test things (meme un peu plus que le minimum imagine toi donc)

--- packages/client/src/app/components/user-profile/user-profile-edit-dialog/user-profile-edit-dialog.component.spec.ts
+++ packages/client/src/app/components/user-profile/user-profile-edit-dialog/user-profile-edit-dialog.component.spec.ts
@@ -1,34 +1,67 @@
-// import { HttpClientTestingModule } from '@angular/common/http/testing';
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { ReactiveFormsModule } from '@angular/forms';
-// import { MatSnackBarModule } from '@angular/material/snack-bar';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { AlertService } from '@app/services/alert-service/alert.service';
-// import { AuthenticationService } from '@app/services/authentication-service/authentication.service';
-// import { UserService } from '@app/services/user-service/user.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SrcsetPipe } from '@app/pipes/srcset/srcset.pipe';
+import { AlertService } from '@app/services/alert-service/alert.service';
+import { AuthenticationService } from '@app/services/authentication-service/authentication.service';
+import { UserService } from '@app/services/user-service/user.service';
 
-// import { UserProfileEditDialogComponent } from './user-profile-edit-dialog.component';
+import { UserProfileEditDialogComponent } from './user-profile-edit-dialog.component';
 
-// describe('UserProfileEditDialogComponent', () => {
-//     let component: UserProfileEditDialogComponent;
-//     let fixture: ComponentFixture<UserProfileEditDialogComponent>;
+describe('UserProfileEditDialogComponent', () => {
+    let component: UserProfileEditDialogComponent;
+    let fixture: ComponentFixture<UserProfileEditDialogComponent>;
+    let dialogRef: jasmine.SpyObj<MatDialogRef<UserProfileEditDialogComponent>>;
 
-//     beforeEach(async () => {
-//         const authenticationService = jasmine.createSpyObj(AuthenticationService, ['signup', 'validateEmail', 'validateUsername']);
-//         await TestBed.configureTestingModule({
-//             declarations: [UserProfileEditDialogComponent],
-//             imports: [HttpClientTestingModule, MatSnackBarModule, ReactiveFormsModule, BrowserAnimationsModule],
-//             providers: [{ provide: AuthenticationService, useValue: authenticationService }, AlertService, UserService],
-//         }).compileComponents();
-//     });
+    beforeEach(async () => {
+        dialogRef = jasmine.createSpyObj(MatDialogRef, ['close']);
 
-//     beforeEach(() => {
-//         fixture = TestBed.createComponent(UserProfileEditDialogComponent);
-//         component = fixture.componentInstance;
-//         fixture.detectChanges();
-//     });
+        await TestBed.configureTestingModule({
+            declarations: [UserProfileEditDialogComponent, SrcsetPipe],
+            imports: [HttpClientTestingModule, MatSnackBarModule, ReactiveFormsModule, BrowserAnimationsModule, MatDialogModule],
+            providers: [
+                {
+                    provide: AuthenticationService,
+                    useValue: jasmine.createSpyObj(AuthenticationService, ['signup', 'validateEmail', 'validateUsername']),
+                },
+                { provide: MAT_DIALOG_DATA, useValue: {} },
+                { provide: MatDialogRef, useValue: dialogRef },
+                AlertService,
+                UserService,
+            ],
+        }).compileComponents();
+    });
 
-//     it('should create', () => {
-//         expect(component).toBeTruthy();
-//     });
-// });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UserProfileEditDialogComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    describe('onSubmit', () => {
+        it('should call close', () => {
+            component.username.setValue('username');
+            component.avatar.setValue('avatar');
+
+            component.onSubmit();
+
+            expect(dialogRef.close).toHaveBeenCalled();
+        });
+    });
+
+    describe('getUsernameError', () => {
+        it('should return undefined if on error', () => {
+            component.username.setValue('username');
+            component.avatar.setValue('avatar');
+
+            expect(component.getUsernameError()).toBeUndefined();
+        });
+    });
+});
