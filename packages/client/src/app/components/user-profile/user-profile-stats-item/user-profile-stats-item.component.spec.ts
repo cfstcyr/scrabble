293edd4a641a6426bf3e6eb857997a28aa26c404
Charles-François St-Cyr <charlesf2001@gmail.com>
2023-03-09 23:31:14 -0500

    lint

--- packages/client/src/app/components/user-profile/user-profile-stats-item/user-profile-stats-item.component.spec.ts
+++ packages/client/src/app/components/user-profile/user-profile-stats-item/user-profile-stats-item.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserProfileStatsItemComponent } from './user-profile-stats-item.component';
 
 describe('UserProfileStatsItemComponent', () => {
-  let component: UserProfileStatsItemComponent;
-  let fixture: ComponentFixture<UserProfileStatsItemComponent>;
+    let component: UserProfileStatsItemComponent;
+    let fixture: ComponentFixture<UserProfileStatsItemComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UserProfileStatsItemComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [UserProfileStatsItemComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UserProfileStatsItemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UserProfileStatsItemComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
