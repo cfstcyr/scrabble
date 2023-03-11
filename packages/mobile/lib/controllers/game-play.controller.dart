936c1dcb8786723970106154dac117ceeb2e8842
rachadchazbek17 <rachadchazbek@gmail.com>
2023-03-11 14:54:23 -0500

    refactor service

--- packages/mobile/lib/controllers/game-play.controller.dart
+++ packages/mobile/lib/controllers/game-play.controller.dart
@@ -1,11 +1,9 @@
-import 'dart:convert';
-import 'dart:developer';
-
 import 'package:http/http.dart';
 import 'package:mobile/classes/actions/action-data.dart';
 import 'package:mobile/classes/game/game-message.dart';
 import 'package:mobile/classes/game/game-update.dart';
 import 'package:mobile/constants/endpoint.constants.dart';
+import 'package:mobile/constants/socket-events/game-events.dart';
 import 'package:mobile/locator.dart';
 import 'package:mobile/services/socket.service.dart';
 import 'package:rxdart/rxdart.dart';
@@ -28,27 +26,27 @@ class GamePlayController {
 
   String? currentGameId;
 
-  final Subject<void> _actionDone$ = PublishSubject();
+  final Subject<void> _actionDone = PublishSubject();
 
-  Stream<void> get actionDoneEvent => _actionDone$.stream;
+  Stream<void> get actionDoneEvent => _actionDone.stream;
 
-  final Subject<GameUpdateData> _gameUpdate$ = PublishSubject();
+  final Subject<GameUpdateData> _gameUpdate = PublishSubject();
 
-  Stream<GameUpdateData> get gameUpdateEvent => _gameUpdate$.stream;
+  Stream<GameUpdateData> get gameUpdateEvent => _gameUpdate.stream;
 
-  final Subject<GameMessage?> _message$ = PublishSubject();
+  final Subject<GameMessage?> _message = PublishSubject();
 
-  Stream<GameMessage?> get messageEvent => _message$.stream;
+  Stream<GameMessage?> get messageEvent => _message.stream;
 
-  final BehaviorSubject<GameUpdateData> gameUpdate$ =
+  final BehaviorSubject<GameUpdateData> gameUpdate =
       BehaviorSubject<GameUpdateData>();
-  final BehaviorSubject<GameMessage?> newMessage$ =
+  final BehaviorSubject<GameMessage?> newMessage =
       BehaviorSubject<GameMessage?>.seeded(null);
   final PublishSubject<void> actionDone$ = PublishSubject<void>();
 
   Future<void> sendAction(ActionData actionData) async {
     Uri endpoint = Uri.parse("$baseEndpoint/$currentGameId/action");
-    post(endpoint, body: actionData).then((_) => _actionDone$.add(null));
+    post(endpoint, body: actionData).then((_) => _actionDone.add(null));
   }
 
   Future<void> leaveGame() async {
@@ -56,40 +54,12 @@ class GamePlayController {
     await delete(endpoint);
   }
 
-  void sendMessage(String gameId, GameMessage message) {
-    post(Uri.parse("$baseEndpoint/$gameId/message"), body: jsonEncode(message))
-        .then((response) {});
-  }
-
-  void sendError(String gameId, GameMessage message) {
-    final endpoint = "$baseEndpoint/$currentGameId/players/error";
-    post(Uri.parse(endpoint), body: jsonEncode(message)).then((response) {});
-  }
-
-  void handleReconnection(String gameId, String newPlayerId) {
-    final endpoint = "$baseEndpoint/$currentGameId/players/reconnect";
-    post(Uri.parse(endpoint), body: jsonEncode({'newPlayerId': newPlayerId}))
-        .then((response) {});
-  }
-
-  void handleDisconnection(String gameId) {
-    final endpoint = "$baseEndpoint/$currentGameId/players/disconnect";
-    delete(Uri.parse(endpoint)).then((response) {
-      handleDisconnectResponse();
-    }).catchError((error) {
-      final errorMessage = error.toString();
-      final statusCode = error.statusCode;
-      if (statusCode != 0) throw Exception(errorMessage);
-    });
-  }
-
   void configureSocket() {
-    socketService.on('gameUpdate', (dynamic newData) {
-      log('gameUpdate');
-      gameUpdate$.add(GameUpdateData.fromJson(newData));
+    socketService.on(GAME_UPDATE_EVENT_NAME, (dynamic newData) {
+      gameUpdate.add(GameUpdateData.fromJson(newData));
     });
-    socketService.on('newMessage', (dynamic newMessage) {
-      newMessage$.add(GameMessage.fromJson(newMessage));
+    socketService.on(GAME_MESSAGE_EVENT_NAME, (dynamic newMessage) {
+      newMessage.add(GameMessage.fromJson(newMessage));
     });
   }
 
