c8cf21eab2b76ed488ba35d2a9dfdb5f3d86c5b8
ahmedsidi9 <70984071+ahmedsidi9@users.noreply.github.com>
2023-03-12 19:30:03 -0400

    Update create-game.constants.dart

--- packages/mobile/lib/constants/create-game.constants.dart
+++ packages/mobile/lib/constants/create-game.constants.dart
@@ -1,6 +1,3 @@
-const String PUBLIC_LABEL_FR = "Public";
-const String PRIVATE_LABEL_FR = "Privé";
-const String PROTECTED_LABEL_FR = "Protégé";
 const String VP_LEVEL_FIELD_TITLE_FR =
     "Choisissez le niveau des joueurs virtuels qui vont compléter les joueurs manquants  ";
 const String VISIBILITY_FIELD_TITLE_FR =
@@ -8,8 +5,6 @@ const String VISIBILITY_FIELD_TITLE_FR =
 const String ROUND_TIME_FIELD_TITLE_FR = "Choisissez le temps par tour: ";
 const String CREATE_GAME_LABEL_FR = "Créer la partie";
 const String CANCEL_CREATION_LABEL_FR = "Annuler";
-const String VP_LEVEL_EXPERT_LABEL_FR = "Expert";
-const String VP_LEVEL_BEGINNER_LABEL_FR = "Débutant";
 const int DEFAULT_TIME = 60;
 const int MAX_TIME = 5;
 const int MIN_TIME = 30;
