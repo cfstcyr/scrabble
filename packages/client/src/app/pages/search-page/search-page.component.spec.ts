bbc3e818ca2becc3864f3493dabc834d68615904
Charles-François St-Cyr <charlesf2001@gmail.com>
2023-03-12 14:04:57 -0400

    fix tests

--- packages/client/src/app/pages/search-page/search-page.component.spec.ts
+++ packages/client/src/app/pages/search-page/search-page.component.spec.ts
@@ -1,25 +1,28 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { SearchPageComponent } from './search-page.component';
 
 describe('SearchPageComponent', () => {
-  let component: SearchPageComponent;
-  let fixture: ComponentFixture<SearchPageComponent>;
+    let component: SearchPageComponent;
+    let fixture: ComponentFixture<SearchPageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SearchPageComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [SearchPageComponent],
+            imports: [HttpClientTestingModule, MatSnackBarModule, MatDialogModule],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SearchPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SearchPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- packages/client/src/app/pages/user-search-result-page/user-search-result-page.component.spec.ts
+++ packages/client/src/app/pages/user-search-result-page/user-search-result-page.component.spec.ts
@@ -1,25 +1,29 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { UserSearchResultPageComponent } from './user-search-result-page.component';
 
 describe('UserSearchResultPageComponent', () => {
-  let component: UserSearchResultPageComponent;
-  let fixture: ComponentFixture<UserSearchResultPageComponent>;
+    let component: UserSearchResultPageComponent;
+    let fixture: ComponentFixture<UserSearchResultPageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UserSearchResultPageComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [UserSearchResultPageComponent],
+            imports: [HttpClientTestingModule, MatSnackBarModule, MatDialogModule, RouterTestingModule],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UserSearchResultPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UserSearchResultPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
