f7f3a0b3c1fe934bd0f4d1428fe68c6f0e026188
ahmedsidi9 <70984071+ahmedsidi9@users.noreply.github.com>
2023-03-11 21:19:51 -0500

    Create create-game.constants.dart

---
+++ packages/mobile/lib/constants/create-game.constants.dart
@@ -0,0 +1,9 @@
+const String PUBLIC_LABEL_FR = "Public";
+const String PRIVATE_LABEL_FR = "Privée";
+const String PROTECTED_LABEL_FR = "Protégée";
+const String VP_LEVEL_FIELD_TITLE_FR = " ";
+const String VISIBILITY_FIELD_TITLE_FR =
+    "Choisissez la visibilité de votre partie: ";
+const String ROUND_TIME_FIELD_TITLE_FR = "Choisissez le temps par tour: ";
+const String CREATE_GAME_LABEL_FR = "Créer la partie";
+const String CANCEL_CREATION_LABEL_FR = "Annuler";
