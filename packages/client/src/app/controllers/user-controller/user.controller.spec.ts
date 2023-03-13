b4e1dc10165ca77582d2f92a0427aaaea4360fc8
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2023-03-13 01:21:08 -0400

    RS: Add 2 of the 4 files

--- packages/client/src/app/controllers/user-controller/user.controller.spec.ts
+++ packages/client/src/app/controllers/user-controller/user.controller.spec.ts
@@ -1,19 +1,19 @@
-// import { HttpClientTestingModule } from '@angular/common/http/testing';
-// import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
 
-// import { UserController } from './user.controller';
+import { UserController } from './user.controller';
 
-// describe('UserController', () => {
-//     let service: UserController;
+describe('UserController', () => {
+    let service: UserController;
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({
-//             imports: [HttpClientTestingModule],
-//         });
-//         service = TestBed.inject(UserController);
-//     });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+        });
+        service = TestBed.inject(UserController);
+    });
 
-//     it('should be created', () => {
-//         expect(service).toBeTruthy();
-//     });
-// });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+});
