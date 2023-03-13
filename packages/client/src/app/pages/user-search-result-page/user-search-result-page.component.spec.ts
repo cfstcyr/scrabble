8a9162a2ac833ef68744a99839e79ce8dd7555a8
Charles-François St-Cyr <charlesf2001@gmail.com>
2023-03-13 14:46:30 -0400

    fix tests

--- packages/client/src/app/pages/user-search-result-page/user-search-result-page.component.spec.ts
+++ packages/client/src/app/pages/user-search-result-page/user-search-result-page.component.spec.ts
@@ -2,7 +2,9 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
 import { UserSearchResultPageComponent } from './user-search-result-page.component';
 
@@ -14,6 +16,14 @@ describe('UserSearchResultPageComponent', () => {
         await TestBed.configureTestingModule({
             declarations: [UserSearchResultPageComponent],
             imports: [HttpClientTestingModule, MatSnackBarModule, MatDialogModule, RouterTestingModule],
+            providers: [
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        params: of({ username: 'my-username' }),
+                    },
+                },
+            ],
         }).compileComponents();
     });
 
