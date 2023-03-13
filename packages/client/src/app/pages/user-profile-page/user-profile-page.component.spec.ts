b173171503cd597b9048a1479c1e96ac4f2fd04f
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2023-03-13 10:07:38 -0400

    RS: try to spy user service

--- packages/client/src/app/pages/user-profile-page/user-profile-page.component.spec.ts
+++ packages/client/src/app/pages/user-profile-page/user-profile-page.component.spec.ts
@@ -1,30 +1,31 @@
-// import { HttpClientTestingModule } from '@angular/common/http/testing';
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { MatDialogModule } from '@angular/material/dialog';
-// import { MatSnackBarModule } from '@angular/material/snack-bar';
-// import { UserService } from '@app/services/user-service/user.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { UserService } from '@app/services/user-service/user.service';
 
-// import { UserProfilePageComponent } from './user-profile-page.component';
+import { UserProfilePageComponent } from './user-profile-page.component';
 
-// describe('UserProfilePageComponent', () => {
-//     let component: UserProfilePageComponent;
-//     let fixture: ComponentFixture<UserProfilePageComponent>;
+describe('UserProfilePageComponent', () => {
+    let component: UserProfilePageComponent;
+    let fixture: ComponentFixture<UserProfilePageComponent>;
+    const userService = jasmine.createSpyObj(UserService, ['updateStatistics', 'updateGameHistory', 'updateServerActions']);
 
-//     beforeEach(async () => {
-//         await TestBed.configureTestingModule({
-//             declarations: [UserProfilePageComponent],
-//             imports: [HttpClientTestingModule, MatSnackBarModule, MatDialogModule],
-//             providers: [UserService],
-//         }).compileComponents();
-//     });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [UserProfilePageComponent],
+            imports: [HttpClientTestingModule, MatSnackBarModule, MatDialogModule],
+            providers: [{ provide: UserService, useValue: userService }],
+        }).compileComponents();
+    });
 
-//     beforeEach(() => {
-//         fixture = TestBed.createComponent(UserProfilePageComponent);
-//         component = fixture.componentInstance;
-//         fixture.detectChanges();
-//     });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UserProfilePageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-//     it('should create', () => {
-//         expect(component).toBeTruthy();
-//     });
-// });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
