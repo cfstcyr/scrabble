932a84c90db56e0d89f952ec98858b2404aa9d75
rachadchazbek17 <rachadchazbek@gmail.com>
2023-03-14 00:25:39 -0400

    refactor

--- packages/mobile/lib/view-methods/group.methods.dart
+++ packages/mobile/lib/view-methods/group.methods.dart
@@ -18,22 +18,19 @@ Stream<List<Group>> get groupStream {
   });
 }
 
+BehaviorSubject<Group> currentGroupUpdate$ = BehaviorSubject<Group>();
+Stream<Group> get currentGroupUpdateStream => currentGroupUpdate$.stream;
+
 void handleGroupsUpdate(dynamic newGroupsJson) {
   List<Group> receivedGroups = List<Group>.from(
       newGroupsJson.map((dynamic group) => Group.fromJson(group)).toList());
   groups$.add(receivedGroups);
 }
 
-Subject<Group> currentGroupUpdate$ = PublishSubject();
-
-Stream<Group> get currentGroupUpdateStream => currentGroupUpdate$.stream;
-
 Subject<PublicUser> rejectedJoinRequest$ = PublishSubject();
-
 Stream<PublicUser> get rejectedStream => rejectedJoinRequest$.stream;
 
 Subject<PublicUser> canceledGroup$ = PublishSubject();
-
 Stream<PublicUser> get canceledStream => canceledGroup$.stream;
 
 Stream<InitializeGameData> get startGameEvent => startGame$.stream;
