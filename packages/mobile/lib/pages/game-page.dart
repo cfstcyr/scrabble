85ecee6c84037ae0a0dca349e19d7b6a8f691675
amine <amine.bourdache@polymtl.ca>
2023-03-14 12:43:09 -0400

    myscaffold usage

--- packages/mobile/lib/pages/game-page.dart
+++ packages/mobile/lib/pages/game-page.dart
@@ -5,6 +5,7 @@ import 'package:mobile/components/game/game_board.dart';
 import 'package:mobile/components/game/game_info.dart';
 import 'package:mobile/components/game/game_timer.dart';
 import 'package:mobile/components/player/players_container.dart';
+import 'package:mobile/components/scaffold-persistance.dart';
 import 'package:mobile/components/tile/tile-rack.dart';
 import 'package:mobile/constants/layout.constants.dart';
 import 'package:mobile/locator.dart';
@@ -37,52 +38,55 @@ class _GamePageState extends State<GamePage> {
               gameService.handleEndGame(context);
             });
           }
-          return Container(
-            color: Colors.grey.shade100,
-            padding: EdgeInsets.all(SPACE_1),
-            child: SafeArea(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                      child: IntrinsicWidth(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Expanded(child: GameBoard()),
-                        TileRack(),
-                      ],
+          return MyScaffold(
+            title: "Game",
+            body: Container(
+              color: Colors.grey.shade100,
+              padding: EdgeInsets.all(SPACE_1),
+              child: SafeArea(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                        child: IntrinsicWidth(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Expanded(child: GameBoard()),
+                          TileRack(),
+                        ],
+                      ),
+                    )),
+                    SizedBox(
+                      width: 425,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          PlayersContainer(),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: GameInfo(
+                                    value: snapshot.data != null
+                                        ? snapshot.data!
+                                            .computeNumberOfTilesLeft()
+                                            .toString()
+                                        : '0',
+                                    name: "Tuiles restantes",
+                                    icon: Icons.font_download),
+                              ),
+                              Expanded(
+                                child: GameTimer(),
+                              ),
+                            ],
+                          ),
+                          Expanded(child: GameMessages()),
+                          GameActions(),
+                        ],
+                      ),
                     ),
-                  )),
-                  SizedBox(
-                    width: 425,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        PlayersContainer(),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: GameInfo(
-                                  value: snapshot.data != null
-                                      ? snapshot.data!
-                                          .computeNumberOfTilesLeft()
-                                          .toString()
-                                      : '0',
-                                  name: "Tuiles restantes",
-                                  icon: Icons.font_download),
-                            ),
-                            Expanded(
-                              child: GameTimer(),
-                            ),
-                          ],
-                        ),
-                        Expanded(child: GameMessages()),
-                        GameActions(),
-                      ],
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           );
