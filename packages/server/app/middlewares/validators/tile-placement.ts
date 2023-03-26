c97a6e5ccc8f5ecdf2d457c90f4c532e34c4d234
rachadchazbek17 <rachadchazbek@gmail.com>
2023-03-26 16:42:47 -0400

    fixed typo [ci skip]

--- packages/server/app/middlewares/validators/tile-placement.ts
+++ packages/server/app/middlewares/validators/tile-placement.ts
@@ -1,4 +1,4 @@
 import { body } from 'express-validator';
 import { validator } from '@app/middlewares/validator';
 
-export const workPlacementValidator = (field: string | string[]) => validator(body(field).exists().bail(), body(field).isArray());
+export const wordPlacementValidator = (field: string | string[]) => validator(body(field).exists().bail(), body(field).isArray());
