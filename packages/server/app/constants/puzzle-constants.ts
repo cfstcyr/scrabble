f9dd428a611e048caa2f9b58f2ad3520dc46fcf7
Charles-François St-Cyr <charlesf2001@gmail.com>
2023-03-25 20:43:41 -0400

    max number of results [ci-skip]

--- packages/server/app/constants/puzzle-constants.ts
+++ packages/server/app/constants/puzzle-constants.ts
@@ -3,6 +3,7 @@ export const MAX_WORD_SIZE = 7;
 export const MIN_WORD_COUNT = 2;
 export const MAX_WORD_COUNT = 4;
 export const SKIP_PLACEMENT_DISTANCE_CUTOFF = 4;
+export const MAX_PLACEMENTS_IN_RESULT = 5;
 
 export const PUZZLE_GENERATOR_NO_PLACEMENT_POSSIBLE = 'Aucun placement possible pour cette combinaison';
 export const PUZZLE_COMPLETE_NO_ACTIVE_PUZZLE = 'Aucun puzzle en cours';
