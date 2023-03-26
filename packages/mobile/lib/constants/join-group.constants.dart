79d9b352103e3b013ce44d10827379ab3f50ae61
ahmedsidi9 <70984071+ahmedsidi9@users.noreply.github.com>
2023-03-26 12:29:39 -0400

    Create join-group.constants.dart

---
+++ packages/mobile/lib/constants/join-group.constants.dart
@@ -0,0 +1,7 @@
+const FULL_GROUP = "Partie pleine";
+const FULL_GROUP_MESSAGE = " Veuillez choisir une autre partie";
+const INVALID_GAME_PASSWORD = "Mot de passe invalide, Veuillez Réesayer";
+const JOIN_GAME_LABEL_FR = "Joindre";
+const GO_BACK_GROUPS = "Retourner aux parties";
+const GAME_STARTED_MESSAGE = "Oups, la partie a commencé entre temps :(";
+const GAME_STARTED = "Oups, la partie a commencé entre temps :(";
