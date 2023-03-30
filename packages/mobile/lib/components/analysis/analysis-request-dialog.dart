3e5516f126113eb4db4b92b7e3dc668cc64ef6f1
Thomas <thomtrepanier@gmail.com>
2023-03-30 13:35:37 -0400

    Use AppCircularSpinner

--- packages/mobile/lib/components/analysis/analysis-request-dialog.dart
+++ packages/mobile/lib/components/analysis/analysis-request-dialog.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile/components/AppCircularSpinner.dart';
 import 'package:mobile/components/LoadingDots.dart';
 import 'package:mobile/components/app_button.dart';
 import 'package:mobile/constants/layout.constants.dart';
@@ -9,7 +9,8 @@ class AnalysisRequestDialog {
   String message;
   Duration? minimumDelay;
 
-  AnalysisRequestDialog({required this.title, required this.message, this.minimumDelay});
+  AnalysisRequestDialog(
+      {required this.title, required this.message, this.minimumDelay});
 
   void openAnalysisRequestDialog(BuildContext context) {
     ThemeData theme = Theme.of(context);
@@ -19,6 +20,9 @@ class AnalysisRequestDialog {
         barrierDismissible: true,
         builder: (BuildContext context) {
           return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            ),
             insetPadding: EdgeInsets.symmetric(vertical: 196, horizontal: 344),
             title: Center(
               child: Text(
@@ -27,20 +31,19 @@ class AnalysisRequestDialog {
                     ?.copyWith(fontWeight: FontWeight.w500),
               ),
             ),
-            titlePadding:
-            EdgeInsets.symmetric(horizontal: SPACE_4 * 4, vertical: SPACE_4),
+            titlePadding: EdgeInsets.symmetric(
+                horizontal: SPACE_4 * 4, vertical: SPACE_4),
             content: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SizedBox(
-                  width: 128,
-                  height: 128,
-                  child: CircularProgressIndicator(
-                    strokeWidth: 12.0,
-                  ),
+                AppCircularSpinner(
+                  isLoading: true,
+                  size: AppCircularSpinnerSize.large,
                 ),
-                SizedBox(height: SPACE_4,),
+                SizedBox(
+                  height: SPACE_4,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
@@ -72,4 +75,3 @@ class AnalysisRequestDialog {
     Navigator.pop(context);
   }
 }
-
