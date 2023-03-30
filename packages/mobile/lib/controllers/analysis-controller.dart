151d588501113f0ba809055fe5e10121d7c271fe
Thomas <thomtrepanier@gmail.com>
2023-03-30 11:56:32 -0400

    cleanup imports

--- packages/mobile/lib/controllers/analysis-controller.dart
+++ packages/mobile/lib/controllers/analysis-controller.dart
@@ -1,28 +1,12 @@
 import 'dart:convert';
-import 'dart:io';
-
 import 'package:http/http.dart';
 import 'package:http_interceptor/http/intercepted_http.dart';
-import 'package:mobile/classes/actions/action-place.dart';
-import 'package:mobile/classes/actions/word-placement.dart';
 import 'package:mobile/classes/analysis/analysis-request.dart';
 import 'package:mobile/classes/analysis/analysis.dart';
-import 'package:mobile/classes/opponent.dart';
-import 'package:mobile/classes/puzzle/puzzle-config.dart';
-import 'package:mobile/classes/user.dart';
-import 'package:mobile/constants/endpoint.constants.dart';
-import 'package:mobile/constants/socket-events/group-events.dart';
-import 'package:mobile/controllers/game-play.controller.dart';
-import 'package:mobile/view-methods/create-lobby-methods.dart';
-import 'package:mobile/view-methods/group.methods.dart';
-import 'package:rxdart/rxdart.dart';
 
-import '../classes/game/game-config.dart';
-import '../classes/group.dart';
-import '../constants/socket-events/game-events.dart';
-import '../locator.dart';
-import '../services/client.dart';
-import '../services/socket.service.dart';
+import 'package:mobile/constants/endpoint.constants.dart';
+import 'package:mobile/locator.dart';
+import 'package:mobile/services/client.dart';
 
 class AnalysisController {
   PersonnalHttpClient httpClient = getIt.get<PersonnalHttpClient>();
@@ -45,19 +29,6 @@ class AnalysisController {
       return AnalysisCompleted(
           idGameHistory: 1, idUser: 1, criticalMoments: []);
     });
-  }
-
-  Future<Response> completePuzzle(WordPlacement placement) async {
-    return http.post(Uri.parse("$endpoint/complete"),
-        body: jsonEncode(placement));
-  }
-
-  Future<Response> abandonPuzzle() async {
-    return http.post(Uri.parse("$endpoint/abandon"));
-  }
-
-  void quitPuzzle() {
-    http.post(Uri.parse("$endpoint/abandon"));
   }
 
   static final AnalysisController _instance =
--- packages/mobile/lib/services/analysis-service.dart
+++ packages/mobile/lib/services/analysis-service.dart
@@ -1,32 +1,7 @@
-import 'dart:convert';
-
-import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:mobile/classes/actions/word-placement.dart';
 import 'package:mobile/classes/analysis/analysis-request.dart';
 import 'package:mobile/classes/analysis/analysis.dart';
-import 'package:mobile/classes/board/board.dart';
-import 'package:mobile/classes/puzzle/puzzle-config.dart';
-import 'package:mobile/classes/puzzle/puzzle-player.dart';
-import 'package:mobile/classes/puzzle/puzzle.dart';
-import 'package:mobile/classes/rounds/round.dart';
-import 'package:mobile/classes/tile/square.dart';
-import 'package:mobile/classes/tile/tile-placement.dart';
-import 'package:mobile/classes/tile/tile-rack.dart';
-import 'package:mobile/classes/tile/tile.dart';
-import 'package:mobile/constants/puzzle-constants.dart';
-import 'package:mobile/constants/socket-constants.dart';
 import 'package:mobile/controllers/analysis-controller.dart';
-import 'package:mobile/controllers/puzzle-controller.dart';
-import 'package:mobile/routes/navigator-key.dart';
-import 'package:mobile/routes/routes.dart';
-import 'package:mobile/services/game-messages.service.dart';
-import 'package:mobile/services/round-service.dart';
-import 'package:mobile/services/socket.service.dart';
-import 'package:mobile/services/user.service.dart';
-import 'package:rxdart/rxdart.dart';
-
-import '../locator.dart';
+import 'package:mobile/locator.dart';
 
 class AnalysisService {
   final AnalysisController _analysisController =
