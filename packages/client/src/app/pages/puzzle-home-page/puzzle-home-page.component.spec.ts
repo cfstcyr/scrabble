9496d4d32bea6116ffa7d5ba843b92b9c1e17186
Charles-François St-Cyr <charlesf2001@gmail.com>
2023-04-04 16:11:47 -0400

    lint

--- packages/client/src/app/pages/puzzle-home-page/puzzle-home-page.component.spec.ts
+++ packages/client/src/app/pages/puzzle-home-page/puzzle-home-page.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PuzzleHomePageComponent } from './puzzle-home-page.component';
 
 describe('PuzzleHomePageComponent', () => {
-  let component: PuzzleHomePageComponent;
-  let fixture: ComponentFixture<PuzzleHomePageComponent>;
+    let component: PuzzleHomePageComponent;
+    let fixture: ComponentFixture<PuzzleHomePageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PuzzleHomePageComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [PuzzleHomePageComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PuzzleHomePageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PuzzleHomePageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
