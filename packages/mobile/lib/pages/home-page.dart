a698d74da60d84580c535b0eedb96918afaab371
Thomas <thomtrepanier@gmail.com>
2023-04-05 11:27:37 -0400

    Home page title en francais

--- packages/mobile/lib/pages/home-page.dart
+++ packages/mobile/lib/pages/home-page.dart
@@ -18,7 +18,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MyScaffold(
-      title: "Home",
+      title: "Accueil",
       body: Container(
         constraints: BoxConstraints.expand(),
         decoration: BoxDecoration(
