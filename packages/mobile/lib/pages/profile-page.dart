bf3381776fd1166d32fda4935576367227fff002
Thomas <thomtrepanier@gmail.com>
2023-04-05 10:55:27 -0400

    Fix user profile picture not updating in profile page

--- packages/mobile/lib/pages/profile-page.dart
+++ packages/mobile/lib/pages/profile-page.dart
@@ -14,36 +14,56 @@ import '../constants/user-constants.dart';
 import '../locator.dart';
 import '../services/user.service.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   ProfilePage({super.key, required this.userSearchResult});
-  final UserService _userService = getIt.get<UserService>();
+
   final PublicUser userSearchResult;
 
-  BehaviorSubject<PublicUser?> user$ = BehaviorSubject.seeded(null);
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final UserService _userService = getIt.get<UserService>();
   bool isLocalUser = false;
-  String title = 'Mon profil';
+  String title = '';
+
+  Stream<PublicUser?> userToShow = Stream.empty();
+
   BehaviorSubject<UserStatistics> statistics$ =
       BehaviorSubject.seeded(DEFAULT_USER_STATISTICS);
+
   BehaviorSubject<List<GameHistory>> gameHistory$ = BehaviorSubject.seeded([]);
 
   @override
-  Widget build(BuildContext context) {
-    if (_userService.user.value?.username == userSearchResult.username) {
-      user$.add(_userService.user.value);
-      isLocalUser = true;
-      _userService.getUserStatistics().then((event) => statistics$.add(event));
-      _userService.getGameHistory().then((event) => gameHistory$.add(event));
-    } else {
-      title = 'Profil de ${userSearchResult.username}';
-      user$.add(userSearchResult);
-      _userService
-          .getProfileByUsername(userSearchResult.username)
-          .then((value) {
-        statistics$.add(value.statistics);
-        gameHistory$.add(value.gameHistory);
-      });
-    }
+  void initState() {
+    super.initState();
 
+    userToShow = _userService.user.switchMap((PublicUser? user) {
+      if (user?.username == widget.userSearchResult.username) {
+        title = 'Mon profil';
+        isLocalUser = true;
+        _userService
+            .getUserStatistics()
+            .then((event) => statistics$.add(event));
+        _userService.getGameHistory().then((event) => gameHistory$.add(event));
+        return Stream.value(user);
+      } else {
+        title = 'Profil de ${widget.userSearchResult.username}';
+        _userService
+            .getProfileByUsername(widget.userSearchResult.username)
+            .then((value) {
+          statistics$.add(value.statistics);
+          gameHistory$.add(value.gameHistory);
+          return Stream.value(widget.userSearchResult);
+        });
+      }
+      return Stream.value(null);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MyScaffold(
       title: title,
       isLocalProfile: isLocalUser,
@@ -54,26 +74,31 @@ class ProfilePage extends StatelessWidget {
             child: Container(
           padding: EdgeInsets.all(SPACE_3),
           child: StreamBuilder<dynamic>(
-            stream: _userProfilePageStream(),
-            builder: (context, snapshot) {
-              PublicUser user = snapshot.hasData ? snapshot.data![0] : UNKNOWN_USER;
-              UserStatistics statistics = snapshot.hasData ? snapshot.data[1] : DEFAULT_USER_STATISTICS;
-              List<GameHistory> gameHistories = snapshot.hasData ? snapshot.data[2] : [];
+              stream: _userProfilePageStream(),
+              builder: (context, snapshot) {
+                PublicUser user =
+                    snapshot.hasData ? snapshot.data![0] : UNKNOWN_USER;
+                UserStatistics statistics = snapshot.hasData
+                    ? snapshot.data[1]
+                    : DEFAULT_USER_STATISTICS;
+                List<GameHistory> gameHistories =
+                    snapshot.hasData ? snapshot.data[2] : [];
 
-              return Column(children: [
-                UserProfileInfo(user: user, isLocalUser: isLocalUser),
-                UserProfileStatistics(statistics: statistics),
-                UserProfileGameHistory(gameHistories: gameHistories),
-                isLocalUser ? UserProfileServerActions() : Container(),
-              ]);
-            }
-          ),
+                return Column(children: [
+                  UserProfileInfo(user: user, isLocalUser: isLocalUser),
+                  UserProfileStatistics(statistics: statistics),
+                  UserProfileGameHistory(gameHistories: gameHistories),
+                  isLocalUser ? UserProfileServerActions() : Container(),
+                ]);
+              }),
         )),
       ),
     );
   }
 
   Stream<dynamic> _userProfilePageStream() {
-    return CombineLatestStream([user$.stream, statistics$.stream, gameHistory$.stream], (values) => values);
+    return CombineLatestStream<dynamic, dynamic>(
+        [userToShow, statistics$.stream, gameHistory$.stream],
+        (values) => values);
   }
 }
