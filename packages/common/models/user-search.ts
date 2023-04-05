000446320dcb2da3b2f80fe507886e322de688fb
Charles-François St-Cyr <charlesf2001@gmail.com>
2023-04-04 21:44:17 -0400

    add achievements to user search

--- packages/common/models/user-search.ts
+++ packages/common/models/user-search.ts
@@ -1,11 +1,13 @@
 import { GameHistoryForUser } from './game-history';
 import { User } from './user';
 import { PublicUserStatistics } from './user-statistics';
+import { UserAchievement } from './achievement';
 
 export type UserSearchItem = Pick<User, 'username' | 'avatar'>;
 export type UserSearchResult = UserSearchItem & {
     gameHistory: GameHistoryForUser[];
     statistics: PublicUserStatistics;
+    achievements: UserAchievement[];
 };
 
 export type UserSearchQueryResult = {
