04874d0b87743acae12ac89837461100d32599d2
ahmedsidi9 <70984071+ahmedsidi9@users.noreply.github.com>
2023-04-14 13:34:29 -0400

    Update group-events.dart

--- packages/mobile/lib/constants/socket-events/group-events.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: constant_identifier_names
-
-const GROUP_UPDATE = 'groupsUpdate';
-const ACCEPTED_IN_GROUP = 'acceptJoinRequest';
-const REJECTED_FROM_GROUP = 'rejectJoinRequest';
-const CANCELED_GROUP = 'cancelledGroup';
-const USER_LEFT_GROUP = 'userLeftGroup';
-const JOIN_REQUEST = 'joinRequest';
-const JOIN_REQUEST_CANCELLED = 'joinRequestCancelled';
