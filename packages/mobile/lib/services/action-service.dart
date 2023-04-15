02a4b7970734c4130a2ed4e69f4f613936ae1c50
Charles-François St-Cyr <charlesf2001@gmail.com>
2023-04-15 19:13:12 -0400

    GL3H23103-361: dont remove tiles on action not successful

--- packages/mobile/lib/services/action-service.dart
+++ packages/mobile/lib/services/action-service.dart
@@ -24,8 +24,8 @@ class ActionService {
   final GameEventService _gameEventService = getIt.get<GameEventService>();
 
   ActionService._privateConstructor() {
-    gameplayController.actionDoneEvent.listen((ResponseResult result) =>
-        _actionProcessed(result));
+    gameplayController.actionDoneEvent
+        .listen((ResponseResult result) => _actionProcessed(result));
   }
 
   static final ActionService _instance = ActionService._privateConstructor();
@@ -37,16 +37,14 @@ class ActionService {
   Future<void> sendAction(ActionType actionType,
       [ActionPayload? payload]) async {
     if (!getIt.get<GameService>().isLocalPlayerPlaying()) {
-      getIt.get<
-          GameMessagesService>().addMessage(GameMessage(
+      getIt.get<GameMessagesService>().addMessage(GameMessage(
           content: CANNOT_PLAY_WHEN_NOT_LOCAL_TURN,
           senderId: SYSTEM_ERROR_ID,
           gameId: gameplayController.currentGameId ?? ''));
       return;
     }
     if (isActionBeingProcessed) {
-      getIt.get<
-          GameMessagesService>().addMessage(GameMessage(
+      getIt.get<GameMessagesService>().addMessage(GameMessage(
           content: SERVER_PROCESSING_ACTION,
           senderId: SYSTEM_ERROR_ID,
           gameId: gameplayController.currentGameId ?? ''));
@@ -61,10 +59,5 @@ class ActionService {
 
   void _actionProcessed(ResponseResult result) {
     _isActionBeingProcessed.add(false);
-
-    if (!result.isSuccess) {
-      // remove tiles on board after an invalid word
-      _gameEventService.add<void>(PUT_BACK_TILES_ON_TILE_RACK, null);
-    }
   }
 }
